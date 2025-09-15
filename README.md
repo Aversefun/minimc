3fe003ee35bbb9425ff0f1de20167c74eb11690d
Arthur Beck <averse.abfun@gmail.com>
2025-09-14 21:17:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# New Rust Project
+
+IMPORTANT: RUN init.sh!
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+//! New Rust project
+#![warn(
+    missing_docs,
+    clippy::missing_docs_in_private_items,
+    clippy::pedantic,
+    clippy::all,
+    clippy::ignore_without_reason
+)]
+
+fn main() -> anyhow::Result<()> {
+    Ok(())
+}
